92e8ff18afc9e1b7a0f1b2ecb32cab3310c16d14
aradjdi <ahmed.radjdi@gmail.com>
2020-07-12 23:19:40 +0200

    Publish 2.0.0

---
+++ config/middleware.js
@@ -0,0 +1,71 @@
+module.exports = {
+  timeout: 100,
+  load: {
+    before: ['responseTime', 'logger', 'cors', 'responses', 'gzip'],
+    order: ['Define the middlewares\' load order by putting their name in this array is the right order'],
+    after: ['parser', 'router'],
+  },
+  settings: {
+    // application.json
+    favicon: {
+      path: 'favicon.ico',
+      maxAge: 86400000,
+    },
+    public: {
+      path: './public',
+      maxAge: 60000,
+    },
+
+    // language.json
+    enabled: true,
+    defaultLocale: 'en_us',
+    modes: [
+      'query',
+      'subdomain',
+      'cookie',
+      'header',
+      'url',
+      'tld'
+    ],
+    cookieName: 'locale',
+
+    // security.json
+    csrf: { enabled: false, key: '_csrf', secret: '_csrfSecret' },
+    csp: { enabled: true, policy: ['block-all-mixed-content'] },
+    p3p: { enabled: true, value: '' },
+    hsts: { enabled: true, maxAge: 31536000, includeSubDomains: true },
+    xframe: { enabled: true, value: 'SAMEORIGIN' },
+    xss: { enabled: true, mode: 'block' },
+    cors: {
+      enabled: true,
+      origin: '*',
+      expose: ['WWW-Authenticate', 'Server-Authorization'],
+      maxAge: 31536000,
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+      headers: ['Content-Type', 'Authorization', 'X-Frame-Options', 'Origin'],
+    },
+    ip: { enabled: false, whiteList: [], blackList: [] },
+
+    // request.json
+    session: {
+      enabled: true,
+      client: 'cookie',
+      key: 'strapi.sid',
+      prefix: 'strapi:sess:',
+      secretKeys: ['mySecretKey1', 'mySecretKey2'],
+      httpOnly: true,
+      maxAge: 86400000,
+      overwrite: true,
+      signed: false,
+      rolling: false
+    },
+    logger: { level: 'debug', exposeInContext: true, requests: true },
+    parser: { enabled: true, multipart: true },
+    
+    // response.json 
+    gzip: { enabled: true },
+    responseTime: { enabled: false },
+    poweredBy: { enabled: true, value: 'Strapi <strapi.io>' },
+  },
+};
---
+++ config/plugins.js
@@ -0,0 +1,15 @@
+if (process.env.NODE_ENV === 'production') {
+  module.exports = ({ env }) => ({
+    provider: 'aws-s3',
+    providerOptions: {
+      accessKeyId: env('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: env('AWS_ACCESS_SECRET'),
+      region: 'eu-west-3',
+      params: {
+        Bucket: 'scb-back-preprod-images',
+      },
+    },
+  });
+} else {
+  module.exports = {};
+}
